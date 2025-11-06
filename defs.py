5387a8988b67e8086c86f2ad2b338b3d73a55a65
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-05 21:39:21 -0500

    fix university path

--- defs.py
+++ defs.py
@@ -4,6 +4,6 @@ from typing import (
 from pathlib import Path as _Path
 FILEPATH_GROCERIES:_Final[_Path] = _Path('datasets/groceries.csv')
 FILEPATH_RECOMMENDATION_DATA:_Final[_Path] = _Path('datasets/g_recommendation_data.csv')
-FILEPATH_UNIVERSITY_RANKINGS:_Final[_Path] = _Path('datasets/university_rankings.csv')
+FILEPATH_UNIVERSITY_RANKINGS:_Final[_Path] = _Path('datasets/Universities.csv')
 FILEPATH_EW_AIRLINE:_Final[_Path] = _Path('datasets/EastWestAirlinesCluster.csv')
 FILEPATH_CEREAL:_Final[_Path] = _Path('datasets/Cereals.csv')
