2270745e2208ad7282fbae67d5baca11753997ca
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-05 21:31:13 -0500

    init the university_rankings package

---
+++ university_rankings/dataset.py
@@ -0,0 +1,11 @@
+from typing import (
+	Optional as _Optional,
+	List as _List,
+)
+import pandas as _pd
+from helpers.dataset import DatasetBase as _DatasetBase
+from helpers.exceptions import DatasetInvalidPath as _DatasetInvalidPath
+
+from defs import (
+	FILEPATH_UNIVERSITY_RANKINGS as _FILEPATH_UNIVERSITY_RANKINGS,
+)
