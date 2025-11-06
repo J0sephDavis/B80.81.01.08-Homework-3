1aa42f4d8de89f5337af0cc94f6b6931fb26902a
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-05 20:36:03 -0500

    define default file paths

---
+++ defs.py
@@ -0,0 +1,6 @@
+from typing import (
+	Final as _Final,
+)
+from pathlib import Path as _Path
+FILEPATH_GROCERIES:_Final[_Path] = _Path('groceries.csv')
+FILEPATH_RECOMMENDATION_DATA:_Final[_Path] = _Path('g_recommendation_data.csv')
