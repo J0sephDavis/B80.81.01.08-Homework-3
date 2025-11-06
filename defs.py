b05df31ca86a855b9323bfe946bf1f419e44dd4d
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-05 21:19:51 -0500

    update IFLEPATH

--- defs.py
+++ defs.py
@@ -2,5 +2,5 @@ from typing import (
 	Final as _Final,
 )
 from pathlib import Path as _Path
-FILEPATH_GROCERIES:_Final[_Path] = _Path('groceries.csv')
-FILEPATH_RECOMMENDATION_DATA:_Final[_Path] = _Path('g_recommendation_data.csv')
+FILEPATH_GROCERIES:_Final[_Path] = _Path('datasets/groceries.csv')
+FILEPATH_RECOMMENDATION_DATA:_Final[_Path] = _Path('datasets/g_recommendation_data.csv')
