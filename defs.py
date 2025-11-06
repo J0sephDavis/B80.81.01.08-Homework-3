2be6a4858b52ea84c05c2095371da16c8a631c2a
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-06 11:11:35 -0500

    define logger names

--- defs.py
+++ defs.py
@@ -11,13 +11,16 @@ Recommendation Data -- The processed dataset
 Frequent Item Set -- The frequent itemsets, for assignment results
 Association Rules -- The rules for analysis
 '''
+app_logger_name:_Final[str] = 'HW3'
 class QuestioneOneData:
+	logger_name:_ClassVar[str] = f'{app_logger_name}.Q1'
 	Groceries:_ClassVar[_Path] = _Path('datasets/q1/groceries.csv') # Raw data
 	PreProcessed:_ClassVar[_Path]=_Path('datasets/q1/recommendation_data.csv') # Hot encoded data
 	FrequentItemSet:_ClassVar[_Path]=_Path('datasets/q1/frequent_item_set.csv') # Meets support
 	AssociatonRules:_ClassVar[_Path]=_Path('datasets/q1/association_rules.csv') # Meets confidence
 
 class QuestionTwoData:
+	logger_name:_ClassVar[str] = f'{app_logger_name}.Q2'
 	Rankings:_ClassVar[_Path]= _Path('datasets/q2/Universities.csv')
 	CleanNormal:_ClassVar[_Path] = _Path('datasets/q2/UCleanNormal.csv')
 	
