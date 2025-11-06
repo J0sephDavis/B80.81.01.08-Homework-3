ae9f149dd7045a484d5f2a59a66bfa066703e9a5
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-06 11:11:43 -0500

    setup logging in main & q2

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from defs import app_logger_name
+from datetime import datetime
+import logging
+logger = logging.getLogger(app_logger_name)
+logger.setLevel(logging.DEBUG)
+LOG_FORMATTER = logging.Formatter(fmt=r'%(levelname)s;%(name)s;%(asctime)s | %(message)s')
+def _setup_handler(handler:logging.Handler):
+	handler.setFormatter(LOG_FORMATTER)
+	logger.addHandler(handler)
+FILE_HANDLER = logging.FileHandler(
+	filename=f'{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {app_logger_name}.log',mode='w'
+)
+FILE_HANDLER.setLevel(logging.DEBUG)
+STREAM_HANDLER = logging.StreamHandler()
+STREAM_HANDLER.setLevel(logging.INFO)
+_setup_handler(FILE_HANDLER)
+_setup_handler(STREAM_HANDLER)
+
 from grocery_recommendation.dataset import question_one
 question_one()
+
+from university_rankings.dataset import question_two
+question_two()
 exit()
