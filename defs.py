068a3c92eef3798fe6995880c0864425ef5f9ace
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-06 09:27:57 -0500

    okay. use a class instead of a dict.

--- defs.py
+++ defs.py
@@ -1,6 +1,7 @@
 from typing import (
 	Final as _Final,
 	Dict as _Dict,
+	ClassVar as _ClassVar
 )
 from pathlib import Path as _Path
 
@@ -10,12 +11,12 @@ Recommendation Data -- The processed dataset
 Frequent Item Set -- The frequent itemsets, for assignment results
 Association Rules -- The rules for analysis
 '''
-QuestionOne:_Final[_Dict[str,_Path]] = {
-	'Groceries':_Path('datasets/q1/groceries.csv'),
-	'Recommendation':_Path('datasets/q1/recommendation_data.csv'),
-	'FrequentItemSet':_Path('datasets/q1/frequent_item_set.csv'),
-	'AssociatonRules':_Path('dataset/q1/association_rules.csv')
-}
+class QuestioneOneData:
+	Groceries:_ClassVar[_Path] = _Path('datasets/q1/groceries.csv') # Raw data
+	PreProcessed=_ClassVar[_Path]('datasets/q1/recommendation_data.csv') # Hot encoded data
+	FrequentItemSet=_ClassVar[_Path]('datasets/q1/frequent_item_set.csv') # Meets support
+	AssociatonRules=_ClassVar[_Path]('dataset/q1/association_rules.csv') # Meets confidence
+
 FILEPATH_UNIVERSITY_RANKINGS:_Final[_Path] = _Path('datasets/Universities.csv')
 FILEPATH_EW_AIRLINE:_Final[_Path] = _Path('datasets/EastWestAirlinesCluster.csv')
 FILEPATH_CEREAL:_Final[_Path] = _Path('datasets/Cereals.csv')
