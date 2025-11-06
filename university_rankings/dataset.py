1f7dbf2e9cefce9a28d8fc496898410605dcdf26
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-05 21:48:39 -0500

    define columns as strenum

--- university_rankings/dataset.py
+++ university_rankings/dataset.py
@@ -19,6 +19,28 @@ class UniversityDataset(_DatasetBase):
 			path=_FILEPATH_UNIVERSITY_RANKINGS,
 			)->None:
 		super().__init__(path)
+	class Columns(_StrEnum):
+		CollegeName = r'College Name'
+		State = r'State'
+		PublicPrivate = r'Public (1)/ Private (2)'
+		AppRec = r'# appli. rec\'d'
+		AppAccept = r'# appl. accepted'
+		NewStudentsEnrolled = r'# new stud. enrolled'
+		NewStudentsFromTop10P = r'% new stud. from top 10%'
+		NewStudentsFromTop25P = r'% new stud. from top 25%'
+		FTUndergrad = r'# FT undergrad'
+		PTUndergrad = r'# PT undergrad'
+		InStateTuition = r'in-state tuition'
+		OutOfStateTuition = r'out-of-state tuition'
+		Room = r'room'
+		Booard = r'board'
+		AdditionalFees = r'add. fees'
+		EstimatedBookCosts = r'estim. book costs'
+		EstimatedPersonalCost = r'estim. personal $'
+		RatioFacultyWithPHD = r'% fac. w/PHD'
+		RatioStudentFaculty = r'stud./fac. ratio'
+		GraduationRate = r'Graduation rate'
+
 	def save(self, clobber:bool=True, include_index:bool=False, **to_csv_kwargs)->None:
 		''' You cannot overwrite the original dataset. '''
 		raise Exception('Forbidden')
