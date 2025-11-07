6f83bbdcf0223cb81dd0b44cf3921a242d98f78b
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-07 13:20:38 -0500

    dendrogram folder in defs

--- defs.py
+++ defs.py
@@ -23,7 +23,7 @@ class QuestionTwoData:
 	logger_name:_ClassVar[str] = f'{app_logger_name}.Q2'
 	Rankings:_ClassVar[_Path]= _Path('datasets/q2/Universities.csv')
 	CleanNormal:_ClassVar[_Path] = _Path('datasets/q2/UCleanNormal.csv')
-	DendrogramFigure:_ClassVar[_Path] = _Path('university_dendrogram.tiff')
+	DendrogramFolder:_ClassVar[_Path] = _Path('q2/dendrograms')
 	
 FILEPATH_EW_AIRLINE:_Final[_Path] = _Path('datasets/EastWestAirlinesCluster.csv')
 FILEPATH_CEREAL:_Final[_Path] = _Path('datasets/Cereals.csv')
