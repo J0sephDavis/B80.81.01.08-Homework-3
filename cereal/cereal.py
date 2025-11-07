f7f32fa60da6c6fe75c22fd75994d31bc8313daf
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-07 17:54:56 -0500

    bulk import

--- cereal/cereal.py
+++ cereal/cereal.py
@@ -1,2 +1,35 @@
+from enum import StrEnum as _StrEnum
+from pathlib import Path as _Path
+from typing import (
+	Optional as _Optional,
+	ClassVar as _ClassVar,
+	Tuple as _Tuple,
+	Dict as _Dict,
+)
+from helpers.plotting import plot_dendrogram as _plot_dendrogram
+import pandas as _pd
+from helpers.dataset import DatasetBase as _DatasetBase
+from sklearn.preprocessing import Normalizer as _Normalizer
+from sklearn.cluster import AgglomerativeClustering as _AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram as _dendrogram
+import numpy as _np
+from defs import QuestionThreeData as _Q3D
+
+from helpers.dataset import (
+	DatasetBase as _DatasetBase,
+	DatasetCSV as _DatasetCSV,
+	DatasetCSVReadOnly as _DatasetCSVReadOnly,
+	DatasetSaveMixin as _DatasetSaveMixin,
+)
+import logging
+logger = logging.getLogger(_Q3D.logger_name)
+_Q3D.
+_Q2D.folder_dendrograms.mkdir(mode=0o775, parents=True,exist_ok=True)
+_Q2D.folder_boxplots.mkdir(mode=0o775, parents=True, exist_ok=True)
+import matplotlib.pyplot as _plt
+from matplotlib.figure import Figure as _Figure
+from matplotlib.axes import Axes as _Axes
+
+
 def question_three():
 	return
