f2655cf03aa374b533614765637350e02657a064
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-08 11:34:55 -0500

    use new dendrogram code in q3

--- cereal/cereal.py
+++ cereal/cereal.py
@@ -83,64 +83,24 @@ class CleanNormalCereal(_DatasetCSV):
 
 class CleanNormalLabeled(_DatasetBase, _DatasetSaveMixin):
 	COLUMN_LABEL:_ClassVar[str] = 'LABEL'
-	default_Path:_ClassVar[_Path] = _Q3D.CerealCleanedNormalLabeled
-	aggcluster_default_args:_ClassVar[_Dict] = {
-		'metric':'euclidean',
-		'compute_full_tree':True,
-		'compute_distances':True,
-		'n_clusters':None, # must be none when distance_threshold is set.
-	}
-	distance_threshold:float
-	model:_AgglomerativeClustering
-	linkage:_Literal['single','complete']
+	default_path:_ClassVar[_Path] = _Q3D.CerealCleanedNormalLabeled
+	dendrogram:_CustomDendrogram
 
-	def __init__(self, cleanCereal:CleanNormalCereal, distance_threshold:float, linkage:_Literal['single','complete']):
+	def __init__(self, cleanCereal:CleanNormalCereal, dendrogram:_CustomDendrogram):
 		logger.debug('CleanNormalLabeled.__init__')
-		super().__init__(path=self.default_Path, frame=None)
-		self.distance_threshold = distance_threshold
-		self.linkage = linkage
-		self.figure_file = _Q3D.folder_dendrograms.joinpath(f'dendrogram linkage={self.linkage} dt={self.distance_threshold:0.2f}.tiff')
-		
-		self.model = _AgglomerativeClustering(
-			distance_threshold=self.distance_threshold,
-			linkage=linkage,
-			**self.aggcluster_default_args
+		super().__init__(
+			path=self.default_path,
+			frame=cleanCereal.get_frame().copy()
 		)
-		self.frame = cleanCereal.get_frame().copy()
-		self.model.fit(self.frame)
-		self.get_frame()[self.COLUMN_LABEL]=self.model.labels_
-	
-	def plotsave_dendrogram(self, save_to_file:bool=True, show:bool=False)->_Tuple[_Figure,_Axes]:
-		logger.debug(f'plot_and_save_dendrogram({self.figure_file},...,...)')
-		return _plot_dendrogram(
-			file=self.figure_file,
-			distance_threshold=self.distance_threshold,
-			model=self.model,
-			save_to_file=save_to_file,
-			show=show,
-			linkage=self.linkage
-		)
-
-def generate_many_dendrograms(cleanData:CleanNormalCereal, save_to_file:bool, show:bool, linkage, thresholds:_List[float])->None:
-	logger.info('cereal.generate_many_dendrograms')
-	CleanNormalLabeled(cleanData, 0, linkage).plotsave_dendrogram(save_to_file=save_to_file,show=show)
-	for it in thresholds:
-		if it==0:# skip
-			continue
-		logger.debug(f'distance_threshold:{it}')
-		data = CleanNormalLabeled(cleanData,it, linkage)
-		if data.figure_file.exists() and save_to_file:
-			logger.info(f'{data.figure_file} already exists. skipping...')
-			continue
-		data.plotsave_dendrogram(save_to_file=save_to_file,show=show)
-
+		self.dendrogram = dendrogram
+		self.get_frame()[self.COLUMN_LABEL] = dendrogram.get_model().labels_
 
 def question_three():
 	cereal = CerealRanking()
 	cerealCN = CleanNormalCereal(cereal=cereal)
 	complete_distance_thresholds=[x/100 for x in range(30, 105, 5)]
 	single_distance_thresholds=[x/100 for x in range(4,32,2)]
-	_generate_many_dendrograms(
+	dendrograms = _generate_many_dendrograms(
 		data=cerealCN.get_frame(),
 		linkage='single',
 		thresholds=single_distance_thresholds,
@@ -150,7 +110,8 @@ def question_three():
 		file_str_fmt= 'linkage={linkage} dt={distance_threshold}.tiff',
 		save_to_file=True,
 	)
-	_generate_many_dendrograms(
+	logger.info('Generated single linkeage dendrograms.')
+	dendrograms.extend(_generate_many_dendrograms(
 		data=cerealCN.get_frame(),
 		linkage='complete',
 		thresholds=complete_distance_thresholds,
@@ -159,9 +120,15 @@ def question_three():
 		folder=_Q3D.folder_dendrograms,
 		file_str_fmt= 'linkage={linkage} dt={distance_threshold}.tiff',
 		save_to_file=True,
-	)
-	logger.info('best split is 0.80')
-	# labeled = CleanNormalLabeled(cleanCereal=cerealCN, distance_threshold=0.8)
-	# labeled.save()
+	))
+	logger.info(f'Generated complete linkeage dendrograms. ({len(dendrograms)})')
+	labeled = CleanNormalLabeled(cleanCereal=cerealCN, dendrogram=_CustomDendrogram(
+		data=cerealCN.get_frame(),
+		distance_threshold=0.80,
+		linkage='complete',
+		file=_Path(''), save_to_file=False,
+		show=False,
+	))
+	labeled.save()
 
 	return
