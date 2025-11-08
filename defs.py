575ac467d7fcd8dc2d2e502d64a26231fbef2e11
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-08 14:15:53 -0500

    rename var

--- defs.py
+++ defs.py
@@ -12,30 +12,30 @@ Frequent Item Set -- The frequent itemsets, for assignment results
 Association Rules -- The rules for analysis
 '''
 app_logger_name:_Final[str] = 'HW3'
-datasets_folder:_Path = _Path('datasets')
+folder_root_datasets:_Path = _Path('datasets')
 class QuestioneOneData:
 	logger_name:_ClassVar[str] = f'{app_logger_name}.Q1'
-	Groceries:_ClassVar[_Path] = datasets_folder.joinpath('q1/groceries.csv') # Raw data
-	PreProcessed:_ClassVar[_Path]=datasets_folder.joinpath('q1/recommendation_data.csv') # Hot encoded data
-	FrequentItemSet:_ClassVar[_Path]=datasets_folder.joinpath('q1/frequent_item_set.csv') # Meets support
-	AssociatonRules:_ClassVar[_Path]=datasets_folder.joinpath('q1/association_rules.csv') # Meets confidence
+	Groceries:_ClassVar[_Path] = folder_root_datasets.joinpath('q1/groceries.csv') # Raw data
+	PreProcessed:_ClassVar[_Path]=folder_root_datasets.joinpath('q1/recommendation_data.csv') # Hot encoded data
+	FrequentItemSet:_ClassVar[_Path]=folder_root_datasets.joinpath('q1/frequent_item_set.csv') # Meets support
+	AssociatonRules:_ClassVar[_Path]=folder_root_datasets.joinpath('q1/association_rules.csv') # Meets confidence
 
 class QuestionTwoData:
 	logger_name:_ClassVar[str] = f'{app_logger_name}.Q2'
-	Rankings:_ClassVar[_Path]= datasets_folder.joinpath('q2/Universities.csv')
-	CleanNormal:_ClassVar[_Path] = datasets_folder.joinpath('q2/UCleanNormal.csv')
-	CleanNormalMean:_ClassVar[_Path] = datasets_folder.joinpath('q2/UCleanNormalMean.csv')
-	CleanNormalMedian:_ClassVar[_Path] = datasets_folder.joinpath('q2/UCleanNormalMedian.csv')
+	Rankings:_ClassVar[_Path]= folder_root_datasets.joinpath('q2/Universities.csv')
+	CleanNormal:_ClassVar[_Path] = folder_root_datasets.joinpath('q2/UCleanNormal.csv')
+	CleanNormalMean:_ClassVar[_Path] = folder_root_datasets.joinpath('q2/UCleanNormalMean.csv')
+	CleanNormalMedian:_ClassVar[_Path] = folder_root_datasets.joinpath('q2/UCleanNormalMedian.csv')
 	folder_dendrograms:_ClassVar[_Path] = _Path('q2/dendrograms')
 	folder_boxplots:_ClassVar[_Path] = _Path('q2/boxplots')
-	CleanNormalLabeled:_ClassVar[_Path] = datasets_folder.joinpath('q2/UCleanNormalLabeled.csv')
-	PrivatePublicLabels:_ClassVar[_Path] = datasets_folder.joinpath('q2/PublicPrivateSummary.csv')
-	StateLabels:_ClassVar[_Path] = datasets_folder.joinpath('q2/StateLabelSummary.csv')
+	CleanNormalLabeled:_ClassVar[_Path] = folder_root_datasets.joinpath('q2/UCleanNormalLabeled.csv')
+	PrivatePublicLabels:_ClassVar[_Path] = folder_root_datasets.joinpath('q2/PublicPrivateSummary.csv')
+	StateLabels:_ClassVar[_Path] = folder_root_datasets.joinpath('q2/StateLabelSummary.csv')
 
 class QuestionThreeData:
 	logger_name:_ClassVar[str] = f'{app_logger_name}.Q3'
 	folder_dendrograms:_ClassVar[_Path] = _Path('q3/dendrograms')
-	folder_datasets:_ClassVar[_Path] = datasets_folder.joinpath('q3')
+	folder_datasets:_ClassVar[_Path] = folder_root_datasets.joinpath('q3')
 
 	Cereal:_ClassVar[_Path] = folder_datasets.joinpath('Cereals.csv')
 	CerealCleanedNormalized:_ClassVar[_Path] = folder_datasets.joinpath('CerealsCleanNormal.csv')
